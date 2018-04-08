8a8754998f3efbcd30e977c2d87841423de491b0
lmalila <38190259+lmalila@users.noreply.github.com>
2018-04-08 22:47:08 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        //comment for second commit
+        System.out.println("Hello World second!");
+
+    }
+}
